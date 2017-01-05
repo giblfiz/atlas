0d84c2cffeefe2608ac0aee2043d5c03b7d844cb
Harry Pottash <giblfiz@gmail.com>
2017-01-05 14:34:30 -0800

    changed update_manager_address to match abi

--- client_public/actions/index.js
+++ client_public/actions/index.js
@@ -2,7 +2,7 @@ import Promise from 'bluebird';
 
 import Web3 from 'web3';
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-const UPDATE_MANAGER_ADDRESS = '0x20deed01059bd6441717a1ffb42debff2eb8d037';
+const UPDATE_MANAGER_ADDRESS = '0xee80b2e89f7cc42e3b433107e989f71858070732';
 import ATLAS_ABI from './atlas_abi.json';
 import UPDATE_MANAGER_ABI from './update_manager_abi.json';
 
